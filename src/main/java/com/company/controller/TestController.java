7b3ce0205a8e25f509c7cb6e1eef80d9209e7e49
bigmangcom <icanseeyou21235>
2022-05-11 11:09:22 +0500

    Test api added

---
+++ src/main/java/com/company/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.company.controller;
+
+import com.company.enums.LangEnum;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/init")
+    public String test() {
+        return "App working";
+    }
+}
